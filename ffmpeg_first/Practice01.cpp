60ab5f915cc092b52898802f09ea6768c129f53a
SBMagar <sbmagar.sbm99@gmail.com>
2020-02-03 11:57:40 +0545

    Add project files.

---
+++ ffmpeg_first/Practice01.cpp
@@ -0,0 +1,65 @@
+﻿
+//
+// Video processing example using FFmpeg
+// Written by Ted Burke - last updated 12-2-2017
+//
+
+#include <stdio.h>
+#include <math.h>
+#include <stdint.h>
+
+// Video resolution
+#define W 1280
+#define H 720
+
+// Allocate a buffer to store one frame
+unsigned char frame[H][W][3] = { 0 };
+
+void main()
+{
+    int x, y, count;
+
+    // Open an input pipe from ffmpeg and an output pipe to a second instance of ffmpeg
+    FILE* pipein = _popen("ffmpeg -i A.mp4 -f s16le -ac 1 -", "r");
+    FILE* pipeout = _popen("ffmpeg -y -f s16le -ar 44100 -ac 1 -i - -vf format=yuv420p -map 0:a -codec:a aac -strict -2 VoutA.mp4", "w");
+    
+    //// Process video frames
+    //while (1)
+    //{
+    //    // Read a frame from the input pipe into the buffer
+    //    count = fread(frame, 1, H * W * 3, pipein);
+
+    //    // If we didn't get a frame of video, we're probably at the end
+    //    if (count != H * W * 3) break;
+
+    //    // Process this frame
+    //    for (y = 0; y < H; ++y) for (x = 0; x < W; ++x)
+    //    {
+    //        // Invert each colour component in every pixel
+    //        frame[y][x][0] = 255 - frame[y][x][0]; // red
+    //        frame[y][x][1] = 255 - frame[y][x][1]; // green
+    //        frame[y][x][2] = 255 - frame[y][x][2]; // blue
+    //    }
+
+    //    // Write this frame to the output pipe
+    //    fwrite(frame, 1, H * W * 3, pipeout);
+    //}
+
+    // Read, modify and write one sample at a time
+    int16_t sample;
+    int n = 0;
+    while (1)
+    {
+        count = fread(&sample, 2, 1, pipein); // read one 2-byte sample
+        if (count != 1) break;
+        ++n;
+        sample = sample * sin(n * 5.0 * 2*M_PI / 44100.0);
+        fwrite(&sample, 2, 1, pipeout);
+    }
+
+    // Flush and close input and output pipes
+    fflush(pipein);
+    _pclose(pipein);
+    fflush(pipeout);
+    _pclose(pipeout);
+}
---
+++ ffmpeg_first/Stream.c
@@ -0,0 +1,175 @@
+﻿#include <stdio.h>
+
+#define __STDC_CONSTANT_MACROS
+
+
+#include "libavformat/avformat.h"
+#include "libavutil/mathematics.h"
+#include "libavutil/time.h"
+#include "libavutil/avutil.h"
+#include "libavcodec/avcodec.h"
+
+
+int main(int argc, char* argv[])
+{
+	AVOutputFormat* ofmt = NULL;
+	//Input AVFormatContext and Output AVFormatContext
+	AVFormatContext* ifmt_ctx = NULL, * ofmt_ctx = NULL;
+	AVPacket pkt;
+	const char* in_filename, * out_filename;
+	int ret, i;
+	int videoindex = -1;
+	int frame_index = 0;
+	int64_t start_time = 0;
+
+	in_filename  = "rtsp://ibz_staff:visitnepal2020@192.168.100.91:5000/video/video.mp4";
+	
+
+	out_filename = "rtp://192.168.100.85:8554";//（Output URL）[RTMP]
+	//out_filename = "rtp://233.233.233.233:6666";//（Output URL）[UDP]
+
+	/*av_register_all();*/
+	//Network
+	avformat_network_init();
+	//Input
+	if ((ret = avformat_open_input(&ifmt_ctx, in_filename, 0, 0)) < 0) {
+		printf("Could not open input file.");
+		goto end;
+	}
+	if ((ret = avformat_find_stream_info(ifmt_ctx, 0)) < 0) {
+		printf("Failed to retrieve input stream information");
+		goto end;
+	}
+
+	for (i = 0; i < ifmt_ctx->nb_streams; i++)
+		if (ifmt_ctx->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO) {
+			videoindex = i;
+			break;
+		}
+
+	av_dump_format(ifmt_ctx, 0, in_filename, 0);
+
+	//Output
+
+	//avformat_alloc_output_context2(&ofmt_ctx, NULL, "flv", out_filename); //RTMP
+	avformat_alloc_output_context2(&ofmt_ctx, NULL, "mpegts", out_filename);//UDP
+
+	if (!ofmt_ctx) {
+		printf("Could not create output context\n");
+		ret = AVERROR_UNKNOWN;
+		goto end;
+	}
+	ofmt = ofmt_ctx->oformat;
+	for (i = 0; i < ifmt_ctx->nb_streams; i++) {
+		//Create output AVStream according to input AVStream
+		AVStream* in_stream = ifmt_ctx->streams[i];
+		AVStream* out_stream = avformat_new_stream(ofmt_ctx, in_stream->codec->codec);
+		if (!out_stream) {
+			printf("Failed allocating output stream\n");
+			ret = AVERROR_UNKNOWN;
+			goto end;
+		}
+		//Copy the settings of AVCodecContext
+
+		AVCodecContext* codec_ctx = avcodec_alloc_context3(ifmt_ctx);
+		ret = avcodec_parameters_to_context(codec_ctx, in_stream->codecpar);
+		if (ret < 0) {
+			printf("Failed to copy in_stream codecpar to codec context\n");
+			goto end;
+		}
+
+		codec_ctx->codec_tag = 0;
+		if (ofmt_ctx->oformat->flags & AVFMT_GLOBALHEADER)
+			codec_ctx->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
+
+		ret = avcodec_parameters_from_context(out_stream->codecpar, codec_ctx);
+		if (ret < 0) {
+			printf("Failed to copy codec context to out_stream codecpar context\n");
+			goto end;
+		}
+	}
+	//Dump Format------------------
+	av_dump_format(ofmt_ctx, 0, out_filename, 1);
+	//Open output URL
+	if (!(ofmt->flags & AVFMT_NOFILE)) {
+		ret = avio_open(&ofmt_ctx->pb, out_filename, AVIO_FLAG_WRITE);
+		if (ret < 0) {
+			printf("Could not open output URL '%s'", out_filename);
+			goto end;
+		}
+	}
+	//Write file header
+	ret = avformat_write_header(ofmt_ctx, NULL);
+	if (ret < 0) {
+		printf("Error occurred when opening output URL\n");
+		goto end;
+	}
+
+	start_time = av_gettime();
+	while (1) {
+		AVStream* in_stream, * out_stream;
+		//Get an AVPacket
+		ret = av_read_frame(ifmt_ctx, &pkt);
+		if (ret < 0)
+			break;
+		//FIX：No PTS (Example: Raw H.264)
+		//Simple Write PTS
+		if (pkt.pts == AV_NOPTS_VALUE) {
+			//Write PTS
+			AVRational time_base1 = ifmt_ctx->streams[videoindex]->time_base;
+			//Duration between 2 frames (us)
+			int64_t calc_duration = (double)AV_TIME_BASE / av_q2d(ifmt_ctx->streams[videoindex]->r_frame_rate);
+			//Parameters
+			pkt.pts = (double)(frame_index * calc_duration) / (double)(av_q2d(time_base1) * AV_TIME_BASE);
+			pkt.dts = pkt.pts;
+			pkt.duration = (double)calc_duration / (double)(av_q2d(time_base1) * AV_TIME_BASE);
+		}
+		//Important:Delay
+		if (pkt.stream_index == videoindex) {
+			AVRational time_base = ifmt_ctx->streams[videoindex]->time_base;
+			AVRational time_base_q = { 1,AV_TIME_BASE };
+			int64_t pts_time = av_rescale_q(pkt.dts, time_base, time_base_q);
+			int64_t now_time = av_gettime() - start_time;
+			if (pts_time > now_time)
+				av_usleep(pts_time - now_time);
+
+		}
+
+		in_stream = ifmt_ctx->streams[pkt.stream_index];
+		out_stream = ofmt_ctx->streams[pkt.stream_index];
+		/* copy packet */
+		//Convert PTS/DTS
+		pkt.pts = av_rescale_q(pkt.pts, in_stream->time_base, out_stream->time_base/*, AVRounding(AV_ROUND_NEAR_INF | AV_ROUND_PASS_MINMAX)*/);
+		pkt.dts = av_rescale_q(pkt.dts, in_stream->time_base, out_stream->time_base/*, AVRounding(AV_ROUND_NEAR_INF | AV_ROUND_PASS_MINMAX)*/);
+		pkt.duration = av_rescale_q(pkt.duration, in_stream->time_base, out_stream->time_base);
+		pkt.pos = -1;
+		//Print to Screen
+		if (pkt.stream_index == videoindex) {
+			printf("Send %8d video frames to output URL\n", frame_index);
+			frame_index++;
+		}
+		//ret = av_write_frame(ofmt_ctx, &pkt);
+		ret = av_interleaved_write_frame(ofmt_ctx, &pkt);
+
+		if (ret < 0) {
+			printf("Error muxing packet\n");
+			break;
+		}
+
+		av_packet_unref(&pkt);
+
+	}
+	//Write file trailer
+	av_write_trailer(ofmt_ctx);
+end:
+	avformat_close_input(&ifmt_ctx);
+	/* close output */
+	if (ofmt_ctx && !(ofmt->flags & AVFMT_NOFILE))
+		avio_close(ofmt_ctx->pb);
+	avformat_free_context(ofmt_ctx);
+	if (ret < 0 && ret != AVERROR_EOF) {
+		printf("Error occurred.\n");
+		return -1;
+	}
+	return 0;
+}
